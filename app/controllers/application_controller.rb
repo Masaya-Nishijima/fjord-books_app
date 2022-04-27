5a61192792eeec12f9a1eb8c18c64180f0a235cd
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-04-27 14:48:20 +0900

    ユーザー情報を追加
    郵便番号 住所 プロフィールの記載欄を、ユーザー作成時と編集時に追加し動作するように設定
    DBのカラム追加
    erbの変更
    パラメータを受けとるように調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,25 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:zip])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:zip, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zip, :address, :profile])
   end
+
+  # private
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
+  # end
+
+  # protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:zip])
+  # end
 end
