8c4bd86dff04d4200340e231644bcdfd042358a8
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-11 11:28:08 +0900

    リファクタリング
    デコレーターへの変数の渡し方を調整

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module UserDecorator
-  def resize_avatar(user)
-    user.avatar.variant(resize: "100x100") if user.avatar.attached?
+  def resize_avatar
+    image_tag  avatar.variant(resize: "100x100") if avatar.attached?
   end
 end
