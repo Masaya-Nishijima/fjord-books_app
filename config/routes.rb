f285c3d03fe4cf894a401da7a26e4fc16232b3f6
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-11 15:10:59 +0900

    コメント欄の表示まで成功

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :reports
+  resources :reports do
+    resource :comments
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
