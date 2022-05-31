866f0d9706009be62de4736d87d36c4364cba707
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-31 09:52:02 +0900

    refactor: rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: %i[github]
 
-  validates :uid, uniqueness: { scope: :provider}, allow_nil: true
+  validates :uid, uniqueness: { scope: :provider }, allow_nil: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
