1c88fed986b28feb8082b823f1dfd6736815651f
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-31 09:06:09 +0900

    refactor: 不要なコメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,4 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :books
   resources :users, only: %i(index show)
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
-  # devise_scope :user do
-  #   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
-  
 end
